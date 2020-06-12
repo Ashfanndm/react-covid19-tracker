4d946b86c969271ce7c06fe1d43bf1e86757fb89
ashfanndm1 <ashfanndm1@gmail.com>
2020-06-12 20:20:48 +0530

    Updates

---
+++ precache-manifest.95c220f35ad409acec468cdf8f8601dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91871700b444685e70efddcbfea7182",
+    "url": "/react-covid19-tracker/index.html"
+  },
+  {
+    "revision": "c06bff54b808accd685b",
+    "url": "/react-covid19-tracker/static/css/main.ae0762fc.chunk.css"
+  },
+  {
+    "revision": "c315def5f60ae954b139",
+    "url": "/react-covid19-tracker/static/js/2.88052145.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid19-tracker/static/js/2.88052145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06bff54b808accd685b",
+    "url": "/react-covid19-tracker/static/js/main.36e9302f.chunk.js"
+  },
+  {
+    "revision": "86b19573f6b692a0eb9f",
+    "url": "/react-covid19-tracker/static/js/runtime-main.fd078dfb.js"
+  },
+  {
+    "revision": "1700d204aaf913e10b6caff0481752dc",
+    "url": "/react-covid19-tracker/static/media/saja.1700d204.jpg"
+  }
+]);
