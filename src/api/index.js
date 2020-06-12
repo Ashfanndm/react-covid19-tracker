3a0e4fb633b85daeee09ce35bfd9ea5952d41202
ashfanndm1 <ashfanndm1@gmail.com>
2020-06-12 20:17:58 +0530

    initial deploy

---
+++ src/api/index.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) =>{
+    let changebleUrl = url;
+
+    if (country){
+        changebleUrl = `${url}/countries/${country}`;
+    }
+    try {
+        const { data: {confirmed,recovered,deaths,lastUpdate} } = await axios.get(changebleUrl);
+
+        return {confirmed,recovered,deaths,lastUpdate};
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const { data  } = await axios.get(`${url}/daily`);
+
+        const modifiedData= data.map((dailyData) => ({
+            confirmed:dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+        return modifiedData;
+    }catch (e) {
+        console.log(e);
+    }
+}
+
+export const fetchCountries = async () =>{
+    try {
+        const { data :{countries} } = await axios.get(`${url}/countries`);
+        return countries.map((country) => country.name )
+    }catch (e) {
+        console.log(e);
+    }
+}
